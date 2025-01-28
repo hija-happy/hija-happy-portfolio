ca0ab4a62bf775841bebc6cac3873ed45c6d8df4
hija-happy <hijahappy2004@gmail.com>
2025-01-28 11:25:29 +0530

    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import React from "react";
+import Home from "./pages/Home";
 import styles from './App.module.css'
 
 function App() {
-  
 
   return (
-   <div className={styles.App}> Hello World</div>
+   <div> <Home /> </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.css";
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+        <h1>Hello, I'm Hija Happy</h1>
+        <p> Welcome to my PortFolio</p>
+    </div>
+  );
+}; 
+
+export default Home
