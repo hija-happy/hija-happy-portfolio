f33640dcd9163fbc2a787f180e907f70b3a061d0
hija-happy <hijahappy2004@gmail.com>
2025-02-13 11:08:31 +0530

    Modified APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Home from "./pages/Home";
-import styles from './App.module.css'
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
-import Navbar from "./components/Navbar";
 import Hobbies from "./pages/Hobbies";
-import Footer from "./components/Footer";
-import Projects from "./pages/Project";
+import Home from "./pages/Home";
+
 
 function App() {
 
